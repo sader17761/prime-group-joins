603bb2479286a95d532da4c02f5a7a1c6d64fd48
Corey Sader <corey.sader@gmail.com>
2017-06-06 19:47:00 -0500

    clean up

--- joins-solution.sql
+++ joins-solution.sql
@@ -14,24 +14,6 @@ JOIN products ON warehouse_product.product_id = products.id
 where description = 'cheetos'
 GROUP BY warehouse, description;
 
--- table products has:
---  id
---  description
-
--- table line_items has:
---  id
---  order_id
---  product_id
-
--- table warehouse has:
---  id
---  warehouse
-
--- table warehouse_product has:
---  product_id
---  warehouse_id
---  on_hand
-
 -- 4. Which warehouses have diet pepsi?
 SELECT warehouse, description FROM warehouse
 JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
@@ -40,12 +22,6 @@ where description = 'diet pepsi'
 GROUP BY warehouse, description;
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
--- table customers has:
---  id, first_name, last_name
--- table addresses has customer_id
--- along with:  street, city, state, zip, address_type
--- table orders has:
---  id, order_date, address_id
 SELECT first_name, last_name, COUNT(customers.id) AS number_of_orders FROM customers
 JOIN addresses ON customers.id = addresses.customer_id
 JOIN orders ON addresses.id = orders.address_id
